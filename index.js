e1b601e5134cec467941dc92519889c9c5243504
kc <kejsi1994@yahoo.com>
2021-03-14 04:14:12 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your code here!
+const element = document.createElement('div');
+
+document.body.appendChild(element);
+
+const ul = document.createElement('ul');
+
+for (let i = 0; i < 3; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+
+element.appendChild(ul);
+
+
+
+
+element.style.fontSize = '24px';
+element.style.marginLeft = '30px';
+element.style.lineHeight = 2;
